e009b49fccf61c323b14ecb4638c6536a4a59fed
Yang <yl4070@rit.edu>
2023-01-01 01:38:13 -0500

    add models

---
+++ scripts/Ariel_seg.jl/model.jl
@@ -0,0 +1,56 @@
+using Flux, FastAI, FastVision
+using Metalhead
+
+
+include("model_blocks.jl") # load unsampling ResBlock  
+
+
+task = SupervisedTask(
+    (Image{2}(), Mask{2}(["p","b"])),
+    (
+        ProjectiveTransforms((64, 64)),
+        ImagePreprocessing(),
+        OneHot()
+    )
+)
+
+resbone = Metalhead.ResNet(34).layers[1:end-1]
+unet = taskmodel(task, resbone)
+
+down = Chain(make_down(3, 196), make_down(196, 3))
+backbone = Chain(down, unet[1][1])
+
+
+model = Chain(
+    backbone,
+    Split(
+        Chain(ResBlock(67, 3), convxlayer(3, 2; ks = 1)), # offset
+        Chain(ResBlock(67, 3), convxlayer(3, 2; ks = 1)), # size
+        Chain(ResBlock(67, 3), convxlayer(3, 1; ks = 1)), # heatmap
+        # Chain(ResBlock(67, 3), convxlayer(3, 2; ks = 1)), # segmentation
+        Chain(ResBlock(67, 3), convdirlayer(3, 2; ks = 1))  # evidential
+    )
+)
+# Total: 274 trainable arrays, 26_993_822 parameters,
+# plus 160 non-trainable, 30_368 parameters, summarysize 103.259 MiB.
+
+x = rand(Float32, 256, 256, 3, 16)
+yhat = model(x)
+
+# y = yhat[2]
+
+
+
+# hg_mod = Chain(
+#     convx(3, 128; stride = 2),
+#     ResBlock(128, 96; stride = 2),
+#     hourglass(96, 128),
+#     hourglass(96, 128), # output channel = 96
+#     Split(
+#         Chain(convx(96, 64), Conv((1,1), 64 => 1, relu)),
+#         Chain(convx(96, 64), Conv((1,1), 64 => 2)),
+#         Chain(convx(96, 64), Conv((1,1), 64 => 2))
+#     )
+# ) |> gpu
+
+
